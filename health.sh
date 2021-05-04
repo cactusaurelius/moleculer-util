d46cd58b69d8fdb3d09d7e5eb539b5de0497c7ae
cactusaurelius <39528088+cactusaurelius@users.noreply.github.com>
2021-05-04 23:55:39 +0300

    shell script

---
+++ health.sh
@@ -0,0 +1,12 @@
+npm list -g | grep moleculer-cli || npm install -g moleculer-cli
+
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/cactusaurelius/moleculer-util/develop/moleculer.config.js --output moleculer.config.js
+# Use $NAMESPACE $TRANSPORTER evironment variables
+moleculer call "\$node.knwtHealth" --transporter nats --ns sea --config moleculer.config.js
+rm moleculer.config.js 
+
+
+# RAW replace github with raw.githubusercontent and delete /blob
+# curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/cactusaurelius/moleculer-telegram/main/src/Telegram.ts --output ./commands/ready.js
+# moleculer connect nats --ns sea --commands ./commands/*.js
+
